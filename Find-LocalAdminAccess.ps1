7ac7a2d3ca4d935e98c1cccf047d4c59c805f685
Rob <61951374+Leo4j@users.noreply.github.com>
2023-11-30 20:18:02 +0000

    Update Find-LocalAdminAccess.ps1

--- Find-LocalAdminAccess.ps1
+++ Find-LocalAdminAccess.ps1
@@ -55,6 +55,8 @@ function Find-LocalAdminAccess {
         	[string]$UserName,
         	[string]$Password,
 		[string]$Command,
+  		[string]$Domain,
+    		[string]$DomainController,
 		[switch]$ShowErrors,
 		[switch]$scsafe,
 		[switch]$NoOutput,
@@ -266,12 +268,7 @@ function Find-LocalAdminAccess {
 			$cred = New-Object System.Management.Automation.PSCredential($UserName, $SecPassword)
 		}
 
-		# Load the scripts into variables
-		if ($Method -eq 'WMI') {
-			$WmiScript = (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/Leo4j/Invoke-WMIRemoting/main/Invoke-WMIRemoting.ps1')
-		}
 		if ($Method -eq 'SMB') {
-			$SmbScript = (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/Leo4j/Invoke-SMBRemoting/main/Invoke-SMBRemoting.ps1')
 			# Initialize a mutex to synchronize access to sc.exe operations
 			if($scsafe){
 				$Mutex = [System.Threading.Mutex]::new($false, 'SCMutex')
@@ -383,3 +380,379 @@ function Find-LocalAdminAccess {
 		Write-Output ""
 	}
 }
+
+$WmiScript = @'
+function Invoke-WMIRemoting {
+	
+	<#
+	
+	.SYNOPSIS
+	Invoke-WMIRemoting Author: Rob LP (@L3o4j)
+	https://github.com/Leo4j/Invoke-WMIRemoting
+	
+	#>
+	
+	param (
+	[Parameter(Mandatory = $true)]
+	[string]$ComputerName,
+	[string]$Command,
+	[string]$UserName,
+	[string]$Password
+	)
+
+ 	$ErrorActionPreference = "SilentlyContinue"
+	$WarningPreference = "SilentlyContinue"
+	
+	if($UserName -AND $Password){
+		$SecPassword = ConvertTo-SecureString $Password -AsPlainText -Force
+		$cred = New-Object System.Management.Automation.PSCredential($UserName,$SecPassword)
+	}
+
+	$ClassID = "Custom_WMI_" + (Get-Random)
+	$KeyID = "CmdGUID"
+	
+	try{	
+		if($UserName -AND $Password){$classExists = Get-WmiObject -Class $ClassID -ComputerName $ComputerName -List -Namespace "root\cimv2" -Credential $cred}
+	 	else{$classExists = Get-WmiObject -Class $ClassID -ComputerName $ComputerName -List -Namespace "root\cimv2"}
+   	} catch {Write-Output "[-] Access Denied"; Write-Output ""; break}
+
+     	try{
+		if (-not $classExists) {
+			
+			if($cred){
+				$connectionOptions = New-Object System.Management.ConnectionOptions
+				if($UserName -AND $Password){
+					$connectionOptions.Username = $UserName
+					$connectionOptions.Password = $Password
+				}
+	
+				$scope = New-Object System.Management.ManagementScope("\\$ComputerName\root\cimv2", $connectionOptions)
+				$scope.Connect()
+				
+				$createNewClass = New-Object System.Management.ManagementClass($scope, [System.Management.ManagementPath]::new(), $null)
+				$createNewClass["__CLASS"] = $ClassID
+				$createNewClass.Properties.Add($KeyID, [System.Management.CimType]::String, $false)
+				$createNewClass.Properties[$KeyID].Qualifiers.Add("Key", $true)
+				$createNewClass.Properties.Add("OutputData", [System.Management.CimType]::String, $false)
+				$createNewClass.Properties.Add("CommandStatus", [System.Management.CimType]::String, $false)
+				$createNewClass.Put() | Out-Null
+			}
+			else{
+				$createNewClass = New-Object System.Management.ManagementClass("\\$ComputerName\root\cimv2", [string]::Empty, $null)
+				$createNewClass["__CLASS"] = $ClassID
+				$createNewClass.Properties.Add($KeyID, [System.Management.CimType]::String, $false)
+				$createNewClass.Properties[$KeyID].Qualifiers.Add("Key", $true)
+				$createNewClass.Properties.Add("OutputData", [System.Management.CimType]::String, $false)
+				$createNewClass.Properties.Add("CommandStatus", [System.Management.CimType]::String, $false)
+				$createNewClass.Put() | Out-Null
+			}
+		}
+  	} catch {Write-Output "[-] Access Denied"; Write-Output ""; break}
+	
+	try{
+		if($cred){$wmiData = Set-WmiInstance -Class $ClassID -ComputerName $ComputerName -Credential $cred}
+		else{$wmiData = Set-WmiInstance -Class $ClassID -ComputerName $ComputerName}
+		
+		$wmiData.GetType() | Out-Null
+		$GuidOutput = ($wmiData | Select-Object -Property $KeyID -ExpandProperty $KeyID)
+		$wmiData.Dispose()
+  	} catch {Write-Output "[-] Access Denied"; Write-Output ""; break}
+
+
+	$RunCmd = {
+	        param ([string]$CmdInput)
+		$resultData = $null
+		$wmiDataOutput = $null
+	        $base64Input = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($CmdInput))
+	        $commandStr = "powershell.exe -NoLogo -NonInteractive -ExecutionPolicy Unrestricted -WindowStyle Hidden -EncodedCommand $base64Input"
+	        $finalCommand = "`$outputData = &$commandStr | Out-String; Get-WmiObject -Class $ClassID -Filter `"$KeyID = '$GuidOutput'`" | Set-WmiInstance -Arguments `@{OutputData = `$outputData; CommandStatus='Completed'} | Out-Null"
+	        $finalCommandBase64 = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($finalCommand))
+	        if($cred){$startProcess = Invoke-WmiMethod -ComputerName $ComputerName -Class Win32_Process -Name Create -Credential $cred -ArgumentList ("powershell.exe -NoLogo -NonInteractive -ExecutionPolicy Unrestricted -WindowStyle Hidden -EncodedCommand " + $finalCommandBase64)}
+		else{$startProcess = Invoke-WmiMethod -ComputerName $ComputerName -Class Win32_Process -Name Create -ArgumentList ("powershell.exe -NoLogo -NonInteractive -ExecutionPolicy Unrestricted -WindowStyle Hidden -EncodedCommand " + $finalCommandBase64)}
+	
+	        if ($startProcess.ReturnValue -ne 0) {
+			throw "Failed to start process on $ComputerName. Return value: $($startProcess.ReturnValue)"
+			return
+		}
+			
+		if ($startProcess.ReturnValue -eq 0) {
+			$elapsedTime = 0
+			$timeout = 60
+			do {
+				Start-Sleep -Seconds 1
+				$elapsedTime++
+				if($cred){$wmiDataOutput = Get-WmiObject -Class $ClassID -ComputerName $ComputerName -Credential $cred -Filter "$KeyID = '$GuidOutput'"}
+				else{$wmiDataOutput = Get-WmiObject -Class $ClassID -ComputerName $ComputerName -Filter "$KeyID = '$GuidOutput'"}
+				if ($wmiDataOutput.CommandStatus -eq "Completed") {
+					break
+				}
+			} while ($elapsedTime -lt $timeout)
+			$resultData = $wmiDataOutput.OutputData
+			$wmiDataOutput.CommandStatus = "NotStarted"
+			$wmiDataOutput.Put() | Out-Null
+			$wmiDataOutput.Dispose()
+			return $resultData
+	        } 
+		else {
+			throw "Failed to run command on $ComputerName."
+			return
+	        }
+	}
+
+	if ($Command) {
+		$finalResult = & $RunCmd -CmdInput $Command
+		Write-Output $finalResult
+	} 
+	else {
+	        do {
+	            $inputFromUser = Read-Host "[$ComputerName]: PS:\>"
+	            if ($inputFromUser -eq 'exit') {
+	                Write-Output ""
+	                break
+	            }
+	            if ($inputFromUser) {
+	                $finalResult = & $RunCmd -CmdInput $inputFromUser
+	                Write-Output $finalResult
+	            }
+	        } while ($true)
+	}
+	
+	
+	if($cred){
+		# Create a CimSession with the provided credentials
+		if($UserName -AND $Password) {
+			$sessionOptions = New-CimSessionOption -Protocol Dcom
+			$cimSession = New-CimSession -Credential $cred -ComputerName $ComputerName -SessionOption $sessionOptions
+		} 
+  		else {$cimSession = New-CimSession -ComputerName $ComputerName}
+
+		# Use the CimSession to delete the class
+		$cimInstance = Get-CimInstance -Namespace "ROOT\CIMV2" -ClassName $ClassID -CimSession $cimSession -ErrorAction SilentlyContinue
+		if ($cimInstance) {Remove-CimInstance -CimInstance $cimInstance}
+
+		# Optionally, remove the session when done
+		$cimSession | Remove-CimSession
+	}
+	else{([wmiclass]"\\$ComputerName\ROOT\CIMV2:$ClassID").Delete()}
+}
+'@
+
+$SmbScript = @'
+function Invoke-SMBRemoting {
+	
+	<#
+
+	.SYNOPSIS
+	Invoke-SMBRemoting Author: Rob LP (@L3o4j)
+	https://github.com/Leo4j/Invoke-SMBRemoting
+	
+	#>
+
+	param (
+		[string]$PipeName,
+		[string]$ComputerName,
+		[string]$ServiceName,
+		[string]$Command,
+		[string]$Timeout = "30000",
+		[switch]$Verbose
+	)
+	
+	$ErrorActionPreference = "SilentlyContinue"
+	$WarningPreference = "SilentlyContinue"
+	Set-Variable MaximumHistoryCount 32767
+	
+	if (-not $ComputerName) {
+		Write-Output " [-] Please specify a Target"
+		return
+	}
+	
+	if(!$PipeName){
+		$randomvalue = ((65..90) + (97..122) | Get-Random -Count 16 | % {[char]$_})
+		$randomvalue = $randomvalue -join ""
+		$PipeName = $randomvalue
+	}
+	
+	if(!$ServiceName){
+		$randomvalue = ((65..90) + (97..122) | Get-Random -Count 16 | % {[char]$_})
+		$randomvalue = $randomvalue -join ""
+		$ServiceName = "Service_" + $randomvalue
+	}
+	
+	$ServerScript = @"
+`$pipeServer = New-Object System.IO.Pipes.NamedPipeServerStream("$PipeName", 'InOut', 1, 'Byte', 'None', 4096, 4096, `$null)
+`$pipeServer.WaitForConnection()
+`$sr = New-Object System.IO.StreamReader(`$pipeServer)
+`$sw = New-Object System.IO.StreamWriter(`$pipeServer)
+while (`$true) {
+	if (-not `$pipeServer.IsConnected) {
+		break
+	}
+	`$command = `$sr.ReadLine()
+	if (`$command -eq "exit") {break} 
+	else {
+		try{
+			`$result = Invoke-Expression `$command | Out-String
+			`$result -split "`n" | ForEach-Object {`$sw.WriteLine(`$_.TrimEnd())}
+		} catch {
+			`$errorMessage = `$_.Exception.Message
+			`$sw.WriteLine(`$errorMessage)
+		}
+		`$sw.WriteLine("###END###")
+		`$sw.Flush()
+	}
+}
+`$pipeServer.Disconnect()
+`$pipeServer.Dispose()
+"@
+	
+	$B64ServerScript = [System.Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($ServerScript))
+	
+	$arguments = "\\$ComputerName create $ServiceName binpath= `"C:\Windows\System32\cmd.exe /c powershell.exe -enc $B64ServerScript`""
+	
+	$startarguments = "\\$ComputerName start $ServiceName"
+	
+	Start-Process sc.exe -ArgumentList $arguments -WindowStyle Hidden
+	
+	Start-Sleep -Milliseconds 1000
+	
+	Start-Process sc.exe -ArgumentList $startarguments -WindowStyle Hidden
+	
+	if($Verbose){
+		Write-Output ""
+		Write-Output " [+] Pipe Name: $PipeName"
+		Write-Output ""
+		Write-Output " [+] Service Name: $ServiceName"
+		Write-Output ""
+		Write-Output " [+] Creating Service on Remote Target..."
+	}
+	#Write-Output ""
+	
+	# Get the current process ID
+	$currentPID = $PID
+	
+	# Embedded monitoring script
+	$monitoringScript = @"
+`$serviceToDelete = "$ServiceName" # Name of the service you want to delete
+`$TargetServer = "$ComputerName"
+`$primaryScriptProcessId = $currentPID
+
+while (`$true) {
+	Start-Sleep -Seconds 5 # Check every 5 seconds
+
+	# Check if the primary script is still running using its Process ID
+	`$process = Get-Process | Where-Object { `$_.Id -eq `$primaryScriptProcessId }
+
+	if (-not `$process) {
+		# If the process is not running, delete the service
+		`$stoparguments = "\\`$TargetServer delete `$serviceToDelete"
+		Start-Process sc.exe -ArgumentList `$stoparguments -WindowStyle Hidden
+		break # Exit the monitoring script
+	}
+}
+"@
+	
+	$b64monitoringScript = [System.Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($monitoringScript))
+	
+	# Execute the embedded monitoring script in a hidden window
+	Start-Process powershell.exe -ArgumentList "-WindowStyle Hidden -NoProfile -ExecutionPolicy Bypass -enc $b64monitoringScript" -WindowStyle Hidden
+	
+	$pipeClient = New-Object System.IO.Pipes.NamedPipeClientStream("$ComputerName", $PipeName, 'InOut')
+	
+ 	try {
+		$pipeClient.Connect($Timeout)
+	} catch [System.TimeoutException] {
+		Write-Output "[$($ComputerName)]: Connection timed out"
+		Write-Output ""
+		return
+	} catch {
+		Write-Output "[$($ComputerName)]: An unexpected error occurred"
+		Write-Output ""
+		return
+	}
+
+	$sr = New-Object System.IO.StreamReader($pipeClient)
+	$sw = New-Object System.IO.StreamWriter($pipeClient)
+
+	$serverOutput = ""
+	
+	if ($Command) {
+		$fullCommand = "$Command 2>&1 | Out-String"
+		$sw.WriteLine($fullCommand)
+		$sw.Flush()
+		while ($true) {
+			$line = $sr.ReadLine()
+			if ($line -eq "###END###") {
+				Write-Output $serverOutput.Trim()
+				Write-Output ""
+				return
+			} else {
+				$serverOutput += "$line`n"
+			}
+		}
+	} 
+	
+	else {
+		while ($true) {
+			
+			# Fetch the actual remote prompt
+			$sw.WriteLine("prompt | Out-String")
+			$sw.Flush()
+			
+			$remotePath = ""
+			while ($true) {
+				$line = $sr.ReadLine()
+
+				if ($line -eq "###END###") {
+					# Remove any extraneous whitespace, newlines etc.
+					$remotePath = $remotePath.Trim()
+					break
+				} else {
+					$remotePath += "$line`n"
+				}
+			}
+			
+			$computerNameOnly = $ComputerName -split '\.' | Select-Object -First 1
+			$promptString = "[$computerNameOnly]: $remotePath "
+			Write-Host -NoNewline $promptString
+			$userCommand = Read-Host
+			
+			if ($userCommand -eq "exit") {
+				Write-Output ""
+					$sw.WriteLine("exit")
+				$sw.Flush()
+				break
+			}
+			
+			elseif($userCommand -ne ""){
+				$fullCommand = "$userCommand 2>&1 | Out-String"
+				$sw.WriteLine($fullCommand)
+				$sw.Flush()
+			}
+			
+			else{
+				continue
+			}
+			
+			#Write-Output ""
+
+			$serverOutput = ""
+			while ($true) {
+				$line = $sr.ReadLine()
+
+				if ($line -eq "###END###") {
+					Write-Output $serverOutput.Trim()
+					Write-Output ""
+					break
+				} else {
+					$serverOutput += "$line`n"
+				}
+			}
+		}
+	}
+
+	$stoparguments = "\\$ComputerName delete $ServiceName"
+	Start-Process sc.exe -ArgumentList $stoparguments -WindowStyle Hidden
+	$pipeClient.Close()
+	$pipeClient.Dispose()
+}
+'@
